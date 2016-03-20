83f5dc09fc6bbde22362c91fbed7d3ac15bcb572
yangjchen <yangjiachen@bogon.dhcp>
2016-03-21 02:46:17 +0800

    1

---
+++ assets/octopus/octopus_edgeActions.js
@@ -0,0 +1,16 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play(0000);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'octopus'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play(0000);});
+//Edge binding end
+})("octopus");
+//Edge symbol end:'octopus'
+})(jQuery,AdobeEdge,"EDGE-12199191");
